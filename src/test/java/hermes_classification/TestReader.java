b01e27a3baa4dce83150bab73e0ca256761c07a7
hermesj <hermesj@philnet-pc0020.philnet.ad.uni-koeln.de>
2017-11-13 13:32:07 +0100

    Renamed

--- src/test/java/hermes_classification/TestReader.java
+++ src/test/java/hermes_classification/TestReader.java
@@ -15,17 +15,33 @@ public class TestReader {
 	@Test
 	public void test() throws IOException {
 		FocusJobs fj = new FocusJobs();
-		List<ClassifyUnit> newAdsFromFile = fj.getCategorizedAdsFromFile(new File("src/main/resources/getIn_JobAdDB.xlsx"), false, new File("src/main/resources/getIn_focuses.xlsx"), false);
-		for (ClassifyUnit focusClassifyUnit : newAdsFromFile) {
+		List<ClassifyUnit> classifiedAds = fj.getCategorizedAdsFromFile
+				(new File("src/main/resources/getIn_JobAdDB.xlsx"), 
+				false, 
+				new File("src/main/resources/getIn_focuses.xlsx"), 
+				false);
+		
+		int javaInTitle = 0;
+
+		for (ClassifyUnit classifiedAd : classifiedAds) {
 			
-			FocusClassifyUnit fcu = (FocusClassifyUnit) focusClassifyUnit;
+			FocusClassifyUnit fcu = (FocusClassifyUnit) classifiedAd;
 			//System.out.println(focusClassifyUnit.getContent());
-			System.out.println(fcu.getDegrees());
-			System.out.println(fcu.getInFocus());
-			System.out.println(fcu.getStudySubjects());
-			System.out.println(fcu.getTitle());
-			System.out.println();
+			
+			String title = fcu.getTitle();
+			title = title.toLowerCase();
+			if(title.contains("java")){
+				javaInTitle++;
+			}
+//			System.out.println(title);
+//			System.out.println(fcu.getStudySubjects());
+//			System.out.println(fcu.getInFocus());
+//			System.out.println(fcu.getDegrees());
+			System.out.println(fcu.getContent());
+			System.out.println("********************************************************************************");
 		}
+		System.out.println("Java Teil des Titels in " + javaInTitle + " Fällen.");
+		System.out.println("Java kam nicht im Titel vor in " + (classifiedAds.size()-javaInTitle ) + " Fällen.");
 	}
 
 }
